36ac42ce4c2279e1f4ae23aa222a51ea6bbb4e52
kohbis <18735471+kohbis@users.noreply.github.com>
2025-06-14 13:54:46 +0900

    fix

--- Formula/spotter.rb
+++ Formula/spotter.rb
@@ -6,13 +6,13 @@ class Spotter < Formula
   version "v0.1.1"
 
   on_macos do
-    url "https://github.com/kohbis/spotter/releases/download/#{version}/spotter-macos.tar.gz" # macos
-    sha256 "e413d160cb46c3da4f6e1158c58fbb93b57780d9a2d8f925416a9f384da826c9"
+    url "https://github.com/kohbis/spotter/releases/download/#{version}/spotter-macos.tar.gz"
+    sha256 "e413d160cb46c3da4f6e1158c58fbb93b57780d9a2d8f925416a9f384da826c9" # macos
   end
 
   on_linux do
-    url "https://github.com/kohbis/spotter/releases/download/#{version}/spotter-linux.tar.gz" # linux
-    sha256 "8727b3fa0d4404ae336f09895e3df016e2b485a5f40d2c43ea9dbced1928e59a"
+    url "https://github.com/kohbis/spotter/releases/download/#{version}/spotter-linux.tar.gz"
+    sha256 "8727b3fa0d4404ae336f09895e3df016e2b485a5f40d2c43ea9dbced1928e59a" # linux
   end
 
   def install
